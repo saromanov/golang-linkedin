fea232d7aae290ce515ac8843aa19c47a497796d
Sergey Romanov <xxsmotur@gmail.com>
2016-02-14 23:58:18 +0500

    Puppet for linkedin

---
+++ linkedin.go
@@ -0,0 +1,24 @@
+package linkedin
+
+import (
+   "encoding/json"
+   "golang.org/x/oauth2"
+   ln "golang.org/x/oauth2/linkedin"
+   "io/ioutil"
+   "net/http"
+)
+
+
+type Linkedin struct {
+
+}
+
+func New(appId, appSecret, redirect string, scopes []string) {
+	lnConfig := &oauth2.Config {
+		ClientID: appId,
+		ClientSecret: appSecret,
+		RedirectURL: redirect,
+		Scopes: scopes,
+		Endpoint: ln.Endpoint
+	}
+}
